a5f335717b84ed1bcda2ebdb7690a45dc5b2bb50
Xinglu Liu <xinglu.liu@covestro.com>
2024-11-05 14:59:29 +0800

    add keywords

--- app/fileops/fileops.py
+++ app/fileops/fileops.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, request, send_from_directory, redirect, url_for, render_template_string, jsonify
 import os
-from PyPDF2 import PdfFileReader
-# from docx import Document
 from app.config import upload_folder,download_folder
 from pdfminer.high_level import extract_text
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+import jieba
+from rake_nltk import Rake
+import nltk
+from collections import Counter, defaultdict
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 
 fileops_bp = Blueprint('file_ops', __name__)
-
+rake_nltk_extractor = Rake()
 
 @fileops_bp.route('/upload', methods=['POST'])
 def upload_file():
@@ -22,11 +26,42 @@ def upload_file():
 
         # 提取PDF中的文本
         doc = extract_text(filepath)
-
-            # 你可以在这里添加将文本保存到数据库或返回给客户端的逻辑
-        return jsonify({'message': 'File uploaded successfully', 'text': doc}), 200
+        word_list = preprocess_and_tokenize_text(doc)
+        keywords = extract_keywords(word_list)
+        return jsonify({
+            'message': 'File uploaded successfully',
+            'text': doc,
+            'keywords': keywords}), 200
     else:
         return jsonify({'error': 'File must be a PDF'}), 400
+
+
+#  预处理文本并使用jieba进行分词
+def preprocess_and_tokenize_text(text):
+    # 清理文本（这里可以根据需要添加更多的清理步骤）
+    text = text.strip()  # 去除前后空白
+    # 使用jieba进行分词
+    jieba.load_userdict(os.path.abspath(os.path.curdir + '/app/static/userdict.txt'))  # 加载自定义词典
+    words = jieba.cut(text)
+    # 将分词结果转换为列表形式（如果需要，可以进一步处理，如去除停用词）
+    word_list = list(words)
+    cleaned_list = [item for item in word_list if item not in ["\n", ", ", '（', '）', '【', '】']]
+    # print(cleaned_list)
+    return cleaned_list
+
+
+# 将分词后的文本转换为字符串，并使用TF-IDF方法提取关键字
+def extract_keywords(word_list, top_n=20):
+    text = ' '.join(word_list)
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([text])
+    feature_names = vectorizer.get_feature_names_out()
+    tfidf_scores = tfidf_matrix.toarray()[0]
+
+    # 获取top_n个关键字
+    top_indices = tfidf_scores.argsort()[::-1][:top_n]
+    keywords = [feature_names[i] for i in top_indices]
+    return keywords
 
 
 @fileops_bp.route('/uploads/<filename>')
